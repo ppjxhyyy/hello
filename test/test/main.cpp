c06db09065b46decabc40639afc9c70159b37b67
ppjxhyyy <ppjxhyyy@gmail.com>
2020-04-08 16:44:51 +0800

    First Commit

---
+++ test/test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by 孙斌 on 2019/12/12.
+//  Copyright © 2019年 sunbin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
